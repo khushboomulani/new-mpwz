c328e3d65d8e8d4cfc57f8df714d3db768440418
khushboo mulani <khushboo.mulani@rediffmail.com>
2022-11-02 15:16:46 +0530

    added src env and single files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  url:'https://mpwzservices2.mpwin.co.in/mpeb_english_backend/' //main url
+  //url:'http://mpwzservices.mpwin.co.in:7070/mpeb_english_backend/'
+  // url:'http://10.98.4.75:8020/' //local url
+
+  // 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 khushboomulani
+
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  url:'https://mpwzservices2.mpwin.co.in/mpeb_english_backend/' //main url
+  //url:'http://mpwzservices.mpwin.co.in:7070/mpeb_english_backend/'
+  // url:'http://10.98.4.75:8020/' //local url
+
+  // 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 prafull-patidar
+
+  // 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 khushboomulani
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
